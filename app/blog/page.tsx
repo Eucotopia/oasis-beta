914ff8b271306209b95712b63db05b81376664fb
liwei <wli@pvetec.com>
2024-03-13 19:11:03 +0800

    123

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,6 @@
 import * as React from "react";
 import {title} from "@/components/primitives";
 import {Card, CardBody, CardFooter, CardHeader, Image} from "@nextui-org/react";
-import {Button} from "@nextui-org/button";
 import {useGetRootCategoriesQuery} from "@/features/api/categoryApi";
 
 export default function App() {
@@ -15,15 +14,17 @@ export default function App() {
             <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 w-screen ">
                 <div className={"flex flex-col items-center"}>
                     <h1 className={title({color: "pink"})}>technology stack</h1>
-                    <div className={"flex flex-row gap-28 justify-between mt-4"}>
+                    <div className={"flex-row grid grid-cols-4 justify-between mt-4"}>
                         {
                             rootCategories?.data.map((item, index) => {
                                     return (
                                         <>
                                             <Card
+                                                isPressable
+                                                onPress={()=>alert(item.name)}
                                                 isFooterBlurred
                                                 radius="lg"
-                                                className="border-none"
+                                                className="border-none mr-20 mb-10"
                                             >
                                                 <CardHeader className={"flex flex-row p-2 justify-center"}>{item.name}</CardHeader>
                                                 <CardBody className={"p-4"}>
@@ -35,12 +36,8 @@ export default function App() {
                                                     />
                                                 </CardBody>
                                                 <CardFooter
-                                                    className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                                                    className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-2 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                                                     <p className="text-tiny text-white/80">Available soon.</p>
-                                                    <Button className="text-tiny text-white bg-black/20" variant="flat" color="default"
-                                                            radius="lg" size="sm">
-                                                        Notify me
-                                                    </Button>
                                                 </CardFooter>
                                             </Card>
                                         </>
